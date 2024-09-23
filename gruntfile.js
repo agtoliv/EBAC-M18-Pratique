b5a676916f07b2d3448d10abc55cd37d0ab10ec9
agtoliv <augustooliveira.silveira@gmail.com>
2024-09-23 20:49:22 -0300

    chore: add initial config of compress less

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,21 @@
 module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        
+
+        less: {
+            development: {
+                files:{
+                    '/dev/styles/main.css': 'src/styles/main.less'
+                }
+            },
+            production: {
+                options: {
+                    compress: true,
+                },
+                files: {
+                    'dist/styles/main.min.css': 'src/styles/main.less'
+                }
+            }
+        }
     })
 }
