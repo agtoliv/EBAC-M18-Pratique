4d71c17437bad779f1e268df7d365847b911f185
agtoliv <augustooliveira.silveira@gmail.com>
2024-09-23 21:09:33 -0300

    chore: replace task works

--- gruntfile.js
+++ gruntfile.js
@@ -16,10 +16,53 @@ module.exports = function(grunt){
                     'dist/styles/main.min.css': 'src/styles/main.less'
                 }
             }
+        },
+        replace: {
+            dev: {
+                options:{
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options:{
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
         }
     })
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-replace');
 
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('build', ['less:production', 'replace']);
 }
